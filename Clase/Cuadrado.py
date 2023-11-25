2192555abda4863203dd6efde9ec6d4e5386c2a7
SantiagoColmenares <santiagocolmeasmc0423@gmail.com>
2023-11-25 18:19:40 -0500

    clase

---
+++ Clase/Cuadrado.py
@@ -0,0 +1,5 @@
+class Cuadrado:
+	def area(self):
+		return "Hola soy el area"
+	def perimetro(self):
+		return "Hola soy el perimetro"
---
+++ Clase/instancias.py
@@ -0,0 +1,9 @@
+from Cuadrado import Cuadrado
+#Insatancias de la clase Cuadrado
+c1 = Cuadrado()
+c2 = Cuadrado()
+c3 = Cuadrado()
+c3 = Cuadrado()
+
+print(c1.area())
+print(c1.perimetro())
