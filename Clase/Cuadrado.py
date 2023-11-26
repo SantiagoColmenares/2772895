e74553739cf16059903e47b832ef579b8bb844ef
SantiagoColmenares <santiagocolmeasmc0423@gmail.com>
2023-11-25 20:06:56 -0500

    Se trabajo con objetos y el principio de encapsulamiento

--- Clase/Cuadrado.py
+++ Clase/Cuadrado.py
@@ -1,5 +1,16 @@
 class Cuadrado:
+
+	def __init__(self, _lado):
+		self._lado = _lado
+
 	def area(self):
-		return "Hola soy el area"
+		return self._lado*self._lado
+
 	def perimetro(self):
-		return "Hola soy el perimetro"
+		return self._lado*4
+
+	def get_lado(self):
+		return self._lado
+
+	def set_lado(self, nuevo_lado):
+		self._lado = nuevo_lado
--- Clase/instancias.py
+++ Clase/instancias.py
@@ -1,9 +1,11 @@
 from Cuadrado import Cuadrado
 #Insatancias de la clase Cuadrado
-c1 = Cuadrado()
-c2 = Cuadrado()
-c3 = Cuadrado()
-c3 = Cuadrado()
+c1 = Cuadrado(8)
+#c2 = Cuadrado(8)
+#c3 = Cuadrado(7)
+#c4 = Cuadrado(20)
 
+#A&P Cuadrado 1
+#print(c1.area())
+c1.set_lado(9)
 print(c1.area())
-print(c1.perimetro())
